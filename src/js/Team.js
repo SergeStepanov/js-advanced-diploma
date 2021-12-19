e99698a2834f8d7af9027df75d3e0dd5c84e2abc
Sergey Stepanov <ser1653285@yandex.ru>
2021-12-19 05:08:30 +0300

    added chars

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,3 +1,20 @@
-export default class Team {
+import Bowman from './Bowman';
+import Swordsman from './Swordsman';
+import Daemon from './Daemon';
+import Magician from './Magician';
+import Undead from './Undead';
+import Vampire from './Vampire';
 
+export default class Team {
+  static getStartUserTeam() {
+    return [new Bowman(1), new Swordsman(1)];
+  }
+
+  static getUserTeam() {
+    return [Bowman, Swordsman, Magician];
+  }
+
+  static getEnemyTeam() { // comp
+    return [Daemon, Undead, Vampire];
+  }
 }
