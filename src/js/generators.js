eda6c42c904f8e5bbb89fdace29e5aa3609d88ee
Sergey Stepanov <ser1653285@yandex.ru>
2021-12-19 04:55:10 +0300

    первый коммит

--- src/js/generators.js
+++ src/js/generators.js
@@ -7,8 +7,17 @@
  */
 export function* characterGenerator(allowedTypes, maxLevel) {
   // TODO: write logic here
+  const type = Math.floor(Math.random() * allowedTypes.length);
+  const level = Math.floor((Math.random() * maxLevel) + 1);
+  yield new allowedTypes[type](level);
 }
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) {
   // TODO: write logic here
+  const team = [];
+  for (let i = 0; i < characterCount; i += 1) {
+    const generator = characterGenerator(allowedTypes, maxLevel);
+    team.push(generator.next().value);
+  }
+  return team;
 }
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,26 @@
+/* eslint-disable no-sequences */
 export function calcTileType(index, boardSize) {
   // TODO: write logic here
-  return 'center';
+  switch (index, boardSize) {
+    case index === 0 && boardSize:
+      return 'top-left';
+    case index === 7 && boardSize:
+      return 'top-right';
+    case index === 56 && boardSize:
+      return 'bottom-left';
+    case index === 63 && boardSize:
+      return 'bottom-right';
+    case index >= 1 && index <= 6 && boardSize:
+      return 'top';
+    case index >= 57 && index <= 62 && boardSize:
+      return 'bottom';
+    case index % 8 === 0 && boardSize:
+      return 'left';
+    case index % 8 === 7 && boardSize:
+      return 'right';
+    default:
+      return 'center';
+  }
 }
 
 export function calcHealthLevel(health) {
